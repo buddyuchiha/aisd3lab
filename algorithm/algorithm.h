f190ae9c8a72c26b09763c25b37c6d2c036559ac
afrodetka <Pro100biv@mail.ru>
2023-12-26 23:08:19 +0400

    Реализация 2 задания, сортировка массивов

--- algorithm/algorithm.h
+++ algorithm/algorithm.h
@@ -1,12 +1,111 @@
 #pragma once
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <stdexcept>
-#include <complex>
 #include <random>
+#include <fstream>
+using namespace std;
 
-int main() {
-	int a = 5;
-	cout << a << endl;
+namespace algorithm {
+	struct stats {
+		size_t comparison_count = 0;
+		size_t copy_count = 0;
+
+		stats& operator+=(const stats a) {
+			comparison_count += a.comparison_count;
+			copy_count += a.copy_count;
+			return *this;
+		}
+	};
+
+	ostream& operator<<(ostream& stream, vector<int>& data) {
+		for (int i = 0; i < data.size(); i++) {
+			stream << data[i] << " ";
+		}
+		return stream;
+	}
+
+	template <typename T>
+	stats BubbleSort(vector<T>& data) {
+		stats stat;
+		int size = data.size();
+		for (int i = 0; i < size - 1; i++) {
+			for (int j = (size - 1); j > i; j--) {
+				stat.comparison_count += 1;
+				if (data[j - 1] > data[j]) {
+					stat.copy_count += 1;
+					T temp = data[j - 1];
+					data[j - 1] = data[j];
+					data[j] = temp;
+				}
+			}
+		}
+		return stat;
+	}
+
+	template <typename T>
+	stats ShakerSort(vector<T>& data) {
+		stats stat;
+		int size = data.size();
+		int left = 0;
+		int right = size - 1;
+		T temp = 0;
+		while (left < right) {
+			for (int i = 0; i < right; i++) {
+				stat.comparison_count += 1;
+				if (data[i] > data[i + 1]) {
+					stat.copy_count += 1;
+					temp = data[i + 1];
+					data[i + 1] = data[i];
+					data[i] = temp;
+				}
+			}
+			right--;
+			for (int j = right; j > left; j--) {
+				stat.comparison_count += 1;
+				if (data[j] < data[j - 1]) {
+					stat.copy_count += 1;
+					temp = data[j - 1];
+					data[j - 1] = data[j];
+					data[j] = temp;
+				}
+			}
+			left++;
+		}
+		return stat;
+	}
+
+	template <typename T>
+	stats CombSort(vector<T>& data) {
+		stats stat;
+		double factor = 1.2473309;
+		int step = data.size() - 1;
+		int size = data.size();
+		while (step >= 1)
+		{
+			for (int i = 0; i + step < size; i++)
+			{
+				stat.comparison_count += 1;
+				if (data[i] > data[i + step])
+				{
+					stat.copy_count += 1;
+					T temp = data[i];
+					data[i] = data[i + step];
+					data[i + step] = temp;
+				}
+			}
+			step /= factor;
+		}
+		return stat;
+	}
+
+	template<typename T>
+	vector<T> random(T a, T b, int n, size_t i) {
+		vector<int> res;
+		mt19937 gen(i);
+		uniform_int_distribution<> distribution(a, b);
+		for (size_t i = 0; i < n; i++) {
+			size_t x = distribution(gen);
+			res.push_back(x);
+		}
+		return res;
+	}
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,172 @@
-#pragma once
 #include <iostream>
-#include <random>
+#include <fstream>
+#include "..\algorithm\algorithm.h"
 using namespace std;
+using namespace algorithm;
 
-template <typename T>
-void BubbleSort(vector<T>& data) {
-	int size = data.size();
-	for (int i = 0; i < size - 1; i++) {
-		for (int j = (size - 1); j > i; j--) {
-			if (data[j - 1] > data[j]) {
-				T temp = data[j - 1];
-				data[j - 1] = data[j];
-				data[j] = temp;
-			}
-		}
-	}
-}
 
-template <typename T>
-void ShakerSort(vector<T>& data) {
-	int size = data.size();
-	int left = 0;
-	int right = size - 1;
-	T temp = 0;
-	while (left < right) {
-		for (int i = 0; i < right; i++) {
-			if (data[i] > data[i + 1]) {
-				temp = data[i + 1];
-				data[i + 1] = data[i];
-				data[i] = temp;
-			}
-		}
-		right--;
-		for (int j = right; j > left; j--) {
-			if (data[j] < data[j - 1]) {
-				temp = data[j - 1];
-				data[j - 1] = data[j];
-				data[j] = temp;
-			}
-		}
-		left++;
-	}
-}
-
-template <typename T>
-void CombSort(vector<T>& data) {
-	int size = data.size();
-	int gap = size;
-	float shrink = 1.3;
-	bool sorted = false;
-
-	while (!sorted) {
-		gap = (gap / shrink) > 1 ? (gap / shrink) : 1;
-		if (gap == 1)
-			sorted = true;
-
-		for (int i = 0; i + gap < size; ++i) {
-			if (data[i] > data[i + gap]) {
-				int temp = data[i];
-				data[i] = data[i + gap];
-				data[i + gap] = temp;
-				sorted = false;
-			}
-		}
-	}
-}
 
 
 int main() {
+	
+	//SORT-TESTS
 	vector<int> a = { 503, 87, 512, 61, 908, 170, 897, 275,653, 426, 154, 509, 612, 677, 765, 703 };
 	BubbleSort(a);
-	int size = a.size();
-	for (int i = 0; i < size; i++) {
-		cout << a[i] << ' ';
-	}
-	cout << '\n' << endl;
+	cout << a << endl;
 	vector<int> b = { 503, 87, 512, 61, 908, 170, 897, 275,653, 426, 154, 509, 612, 677, 765, 703 };
 	ShakerSort(b);
-	int _size = b.size();
-	for (int i = 0; i < _size; i++) {
-		cout << b[i] << ' ';
-	}
-	cout << '\n' << endl;
+	cout << b << endl;
 	vector<int> c = { 503, 87, 512, 61, 908, 170, 897, 275,653, 426, 154, 509, 612, 677, 765, 703 };
 	CombSort(c);
-	int __size = c.size();
-	for (int i = 0; i < __size; i++) {
-		cout << c[i] << ' ';
+	cout << c << endl;
+	
+
+
+	// ShakerSort 
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\ShakerSort.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	for (size_t j = 0; j < 100; j++) {
+	//		auto c = random(-55000, 55000, a[i], i);
+	//		b += (ShakerSort(c));
+	//	}
+	//	cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count / 100 << ' ' << b.copy_count / 100 << endl;
+	//}
+	//fout.close();
+	
+	
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\ShakerSortBest.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	vector<int> temp(a[i]);
+	//	for (size_t j = 0; j < a[i]; j++) {
+	//		temp[j] = j;
+	//	}
+	//	b = ShakerSort(temp);
+	// cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count << ' ' << b.copy_count << endl;
+	//}
+	//fout.close();
+	
+	
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\ShakerSortWorst.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	vector<int> temp(a[i]);
+	//	for (size_t j = 0; j < a[i]; j++) {
+	//		temp[j] = j;
+	//	}
+	//	reverse(temp.begin(), temp.end());
+	//	b = ShakerSort(temp);
+	// cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count << ' ' << b.copy_count << endl;
+	//}
+	//fout.close();
+
+
+
+
+	// BubbleSort 
+	/*int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	ofstream fout;
+	fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\BubbleSort.txt");
+	for (size_t i = 0; i < 13; i++) {
+		stats b;
+		for (size_t j = 0; j < 100; j++) {
+			auto c = random(-55000, 55000, a[i], i);
+			b += (BubbleSort(c));
+		}
+		cout << i << endl;
+		fout << a[i] << ' ' << b.comparison_count / 100 << ' ' << b.copy_count / 100 << endl;
 	}
+	fout.close();*/
+	
+	
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\BubbleSortBest.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	vector<int> temp(a[i]);
+	//	for (size_t j = 0; j < a[i]; j++) {
+	//		temp[j] = j;
+	//	}
+	//	b = BubbleSort(temp);
+	//	cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count << ' ' << b.copy_count << endl;
+	//}
+	
+
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\BubbleSortWorst.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	vector<int> temp(a[i]);
+	//	for (size_t j = 0; j < a[i]; j++) {
+	//		temp[j] = j;
+	//	}
+	//	reverse(temp.begin(), temp.end());
+	//	b = BubbleSort(temp);
+	//	cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count << ' ' << b.copy_count << endl;
+	//}
+	//fout.close();
+
+	
+	//CombSort 
+
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\CombSort.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	for (size_t j = 0; j < 100; j++) {
+	//		auto c = random(-55000, 55000, a[i], i);
+	//		b += (CombSort(c));
+	//	}
+	//	cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count / 100 << ' ' << b.copy_count / 100 << endl;
+	//}
+	//fout.close();
+	
+
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\CombSortBest.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	vector<int> temp(a[i]);
+	//	for (size_t j = 0; j < a[i]; j++) {
+	//		temp[j] = j;
+	//	}
+	//	b = CombSort(temp);
+	//	cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count << ' ' << b.copy_count << endl;
+	//}
+
+	//int a[] = { 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 25000, 50000, 100000 };
+	//ofstream fout;
+	//fout.open("C:\\Users\\afrodetka\\Source\\Repos\\aisd3lab\\CombSortWorst.txt");
+	//for (size_t i = 0; i < 13; i++) {
+	//	stats b;
+	//	vector<int> temp(a[i]);
+	//	for (size_t j = 0; j < a[i]; j++) {
+	//		temp[j] = j;
+	//	}
+	//	reverse(temp.begin(), temp.end());
+	//	b = CombSort(temp);
+	//	cout << i << endl;
+	//	fout << a[i] << ' ' << b.comparison_count << ' ' << b.copy_count << endl;
+	//}
+	//fout.close();
+
 }
+	
